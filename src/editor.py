478743fd4adc925c10b061ec882275ddc89b6355
Michael Woods <michaelthomaswoods@gmail.com>
2024-05-20 01:24:48 -0700

    Added background panning and menu in Editor

--- src/editor.py
+++ src/editor.py
@@ -1,18 +1,87 @@
-import pygame, sys 
+import sys
+from pygame.math import Vector2 as vector
+from pygame.mouse import get_pressed as mouse_btns
+from pygame.mouse import get_pos as mouse_pos
 from settings import *
+from menu import Menu
+
 
 class Editor:
-	def __init__(self):
+    def __init__(self):
+        # main setup 
+        self.display_surface = pygame.display.get_surface()
 
-		# main setup 
-		self.display_surface = pygame.display.get_surface()
+        # navigation
+        self.origin = vector()
+        self.pan_active = False
+        self.pan_offset = vector(0,0)
+        # support lines
+        self.support_line_surf = pygame.Surface((WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.support_line_surf.set_colorkey('green')
+        self.support_line_surf.set_alpha(30)
+        # selection
+        self.selection_index = 2
+        self.menu = Menu()
 
-	def event_loop(self):
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
+    # input
+    def event_loop(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            self.pan_input(event)
+            self.selection_hotkeys(event)
+            self.menu_click(event)
 
-	def run(self, dt):
-		self.display_surface.fill('white')
-		self.event_loop()
+    def pan_input(self, event):
+        # middle mouse button pressed /released
+        if event.type == pygame.MOUSEBUTTONDOWN and mouse_btns()[1]:
+            self.pan_active = True
+            self.pan_offset = vector(mouse_pos()) - self.origin
+        if not mouse_btns()[1]:
+            self.pan_active = False
+        # mousewheel
+        if event.type == pygame.MOUSEWHEEL:
+            keys=pygame.key.get_pressed()
+            if keys[pygame.K_LCTRL]:
+                self.origin.y -= event.y * 50
+            else: self.origin.x -= event.y * 50       
+        # panning update
+        if self.pan_active:
+            self.origin = vector(mouse_pos()) - self.pan_offset
+
+    def selection_hotkeys(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.selection_index += 1
+            if event.key == pygame.K_LEFT:
+                self.selection_index -= 1
+        self.selection_index = max(min(self.selection_index, 18), 2)
+
+    def menu_click(self,event):
+        if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint((mouse_pos())):
+            self.selection_index = self.menu.click(mouse_pos(),mouse_btns())
+
+    def draw_tile_lines(self):
+        cols, rows = WINDOW_WIDTH//TILE_SIZE, WINDOW_HEIGHT//TILE_SIZE
+        origin_offset = vector(
+            x = self.origin.x - int(self.origin.x/TILE_SIZE)*TILE_SIZE , 
+            y = self.origin.y - int(self.origin.y/TILE_SIZE)*TILE_SIZE
+        )
+        self.support_line_surf.fill('green')
+        for col in range(cols + 1):
+            x = origin_offset.x + col * TILE_SIZE
+            pygame.draw.line(self.support_line_surf, LINE_COLOR, (x,0), (x,WINDOW_HEIGHT))
+        for row in range(rows + 1):
+            y = origin_offset.y + row * TILE_SIZE
+            pygame.draw.line(self.support_line_surf, LINE_COLOR, (0,y), (WINDOW_WIDTH,y))
+        self.display_surface.blit(self.support_line_surf, (0,0))
+
+    def run(self, dt):
+        self.event_loop()
+
+        # drawing
+        self.display_surface.fill('gray')
+        self.draw_tile_lines()
+        pygame.draw.circle(self.display_surface,'red', self.origin, 10)
+        self.menu.display(self.selection_index)
--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
-import pygame
+import os
+from pygame.image import load 
 from settings import *
-
 from editor import Editor
 
 class Main:
-	def __init__(self):
-		pygame.init()
-		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		self.clock = pygame.time.Clock()
+    def __init__(self):
+        working_dir = os.path.dirname(__file__)
+        os.chdir(working_dir)
+    
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
 
-		self.editor = Editor()
+        self.editor = Editor()
 
-	def run(self):
-		while True:
-			dt = self.clock.tick() / 1000
-			
-			self.editor.run(dt)
-			pygame.display.update()
+        # cursor
+        surf = load('../graphics/cursors/mouse.png').convert_alpha()
+        cursor = pygame.cursors.Cursor((0, 0), surf)
+        pygame.mouse.set_cursor(cursor)
+
+    def run(self):
+        while True:
+            dt = self.clock.tick() / 1000
+            self.editor.run(dt)
+            pygame.display.update()
 
 
 if __name__ == '__main__':
-	main = Main()
-	main.run() 
+    main = Main()
+    main.run() 
